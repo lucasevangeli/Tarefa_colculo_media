363c4892569ec8cdf51b17601afee6d0d897bf1d
Lucas Evan <lucasevan478@gmail.com>
2024-07-05 09:11:53 -0300

    tarefa para numeros

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int num1 = 10;
+        int num2 = 9;
+        int num3 = 5;
+        int num4 = 10;
+
+        int soma = (num1 + num2 + num3 + num4)/2;
+        System.out.println(soma);
+    }
+}
